86ad6b6eb7f37aaeb65651cf6e58795b47759bca
FLOWZ <ketuimevbore@yahoo.com>
2020-01-21 16:52:45 -0800

    dataBinding with onClick method,onLongClick method and click method with parameter

--- app/src/main/java/com/flowz/databindingdemo/MainActivity.java
+++ app/src/main/java/com/flowz/databindingdemo/MainActivity.java
@@ -3,7 +3,10 @@ package com.flowz.databindingdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.flowz.databindingdemo.databinding.ActivityMainBinding;
 import com.flowz.databindingdemo.entity.Contact;
@@ -11,6 +14,7 @@ import com.flowz.databindingdemo.entity.Contact;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
+    private ClickHandler handler;
 
 
 
@@ -24,7 +28,38 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setContact(contact);
 
+        handler = new ClickHandler(this);
+        activityMainBinding.setClickHandler(handler);
+    }
+
+
+    public class ClickHandler {
+        private Context context;
+
+        public ClickHandler(Context context){
+            this.context = context;
+        }
+
+        public void simpleButtonClick (View view){
+            Toast.makeText(context, "This ia a simple onClick with DataBinding", Toast.LENGTH_SHORT).show();
+        }
+
+        public boolean simpleButtonLongClick (View view){
+            Toast.makeText(context, "This ia a simple onLongClick with DataBinding", Toast.LENGTH_SHORT).show();
+            return false;
+
+        }
+
+        public boolean simpleButtonClickwithParameters (View view, Contact contact){
+            Toast.makeText(context, "This ia a simple Button Click method with parameters using DataBinding \n"+
+                    "Name: " +contact.name +" \n" +
+                    "email : " +contact.email,  Toast.LENGTH_SHORT).show();
+            return false;
+
+        }
 
 
     }
 }
+
+
